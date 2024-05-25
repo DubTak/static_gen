a73e03fdedab6e4bcb0eb476ca03db0ae07c9430
dubtak <gabrielhadley@gmail.com>
2024-05-25 01:49:44 -0700

    Update htmlnode.py with LeafNode class and Update various to integrate

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -12,10 +12,10 @@ class HTMLNode:
         """
     
     def to_html(self):
-        raise NotImplementedError
+        raise NotImplementedError('to_html method not implemented')
     
     def props_to_html(self):
-        if self.props == None:
+        if self.props is None:
             return ''
         else:
             props_html = ''
@@ -25,3 +25,18 @@ class HTMLNode:
     
     def __repr__(self):
         return f'HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})'
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError('Invalid HTML: no value')
+        if self.tag is None:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+    
+    def __repr__(self):
+        return f'LeafNode({self.tag}, {self.value}, {self.props})'
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from textnode import TextNode
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 def main():
     test_node = TextNode('This is a text node', 'bold', 'https://www.boot.dev')
@@ -13,6 +13,10 @@ def main():
         )
     print(test_html_node)
     print(f'Testing .props_to_html: {test_html_node.props_to_html()}')
+
+    test_leaf_node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+    print(test_leaf_node)
+    print(f'Testing .to_html(): {test_leaf_node.to_html()}')
 
 
 main()
